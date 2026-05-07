d0631551af664fb0df6e6fd7e27219ef52677130
Venturex <dev.venturex@gmail.com>
2026-05-07 16:31:14 +0200

    Initial project setup

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::net::SocketAddr;
+
+use axum::Router;
+use tokio::net::TcpListener;
+use tower_http::{compression::CompressionLayer, trace::TraceLayer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::fmt::layer()
+                .json()
+                .with_target(true)
+                .with_current_span(true)   
+        )
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into()),
+        ))
+        .init();
+
+    let router = Router::new()
+        .layer(TraceLayer::new_for_http())
+        .layer(CompressionLayer::new());
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let listener = TcpListener::bind(addr).await?;
+
+    tracing::info!("Server running on http://{addr}");
+    axum::serve(listener, router).await?;
+    Ok(())
+}
